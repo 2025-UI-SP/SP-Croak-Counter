3ea6253710412e12d53bc19d41a47c1f8c2ca1a1
AtlasThe8th <duncanpatrick123@gmail.com>
2025-10-21 00:11:38 -0400

    Fixes and edits to about page content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,8 @@ import {
   AccordionSummary, 
   AccordionDetails,
   Box,
-  Link
+  Link,
+  ListItem
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { usePageTitle } from '../hooks/usePageTitle.js';
@@ -37,7 +38,7 @@ function About() {
           textAlign: 'center'
         }}
       >
-        Learn more about the Keweenaw Bay Indian Community (KBIC), their Department of Natural Resources (DNR), and the developers behind this project.
+        Learn more about the Keweenaw Bay Indian Community, their Natural Resources Department, and the developers behind this project.
       </Typography>
       
       <Box sx={{ mt: 4 }}>
@@ -50,46 +51,50 @@ function About() {
             <Typography
               sx={{ fontSize: { xs: '1.125rem', sm: '1.25rem', md: '1.35rem' } }}
             >
-              The <strong>Keweenaw Bay Indian Community (KBIC)</strong> is a federally recognized Native American tribe 
-              located in Michigan’s Upper Peninsula. The community is dedicated to preserving its rich cultural heritage, 
-              protecting its natural resources, and supporting the well-being of its members through programs that emphasize 
-              education, environmental stewardship, and sustainable development.
+              The Keweenaw Bay Indian Community (KBIC) is a federally recognized Tribe and is signatory to the Treaties with the Chippewa of 1842 and 1854. 
+              The members and descendants of the Tribe retain their right to fish, hunt, and gather throughout the ceded territory under the Treaty of 1842. 
+              Established under the Treaty of 1854, the primary land base of the KBIC is the L'Anse Indian Reservation. KBIC also has land holdings in Ontonagon and Marquette Counties. 
               <br /><br />
-              For more information, visit the official documentation:{" "}
+              The L'Anse Indian Reservation consists of approximately 59,000 acres, which is heavily forested. This is a water-rich community, with approximately 23 miles of Lake Superior shoreline, 
+              over 200 miles of streams and rivers, and close to 5,000 acres of inland lakes and wetlands. These diverse habitats sustain a healthy population of fish, wildlife, and plant species. 
+              In addition to recognizing our Treaties with the Federal Government, we honor our First Treaty with all orders of creation, which includes our obligations and connections to the natural environment. 
+              It is our responsibility to speak for those that do not have a voice in the Council of Men.
+              <br /><br />
+              For more information, visit the official website:{" "}
               <Link 
-                href="https://docs.google.com/document/d/1pXOYjwFcwWBPmFB5Dk2TXa9guVWlfPmsg7TfIZZoe9Y/edit?tab=t.0#heading=h.qlly7uutob1m" 
+                href="https://www.kbic-nsn.gov/" 
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                KBIC Overview
+                KBIC Website
               </Link>
             </Typography>
           </AccordionDetails>
         </Accordion>
 
-        {/* KBIC DNR Accordion */}
+        {/* KBIC NRD Accordion */}
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography variant="h5"><strong>KBIC Department of Natural Resources (DNR)</strong></Typography>
+            <Typography variant="h5"><strong>KBIC Natural Resources Department</strong></Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography
               sx={{ fontSize: { xs: '1.125rem', sm: '1.25rem', md: '1.35rem' } }}
             >
-              The <strong>KBIC Department of Natural Resources (DNR)</strong> manages, protects, and restores the 
-              natural resources within the KBIC reservation and surrounding areas. The DNR’s mission focuses on 
+              The <strong>KBIC Natural Resources Department</strong> manages, protects, and restores the 
+              natural resources within the KBIC reservation and surrounding areas. The NRD’s mission focuses on 
               environmental protection, wildlife conservation, fisheries enhancement, and maintaining ecological 
               balance for future generations.
               <br /><br />
-              Visit the DNR website to learn more about their ongoing projects and programs:{" "}
+              Visit the NRD's website to learn more about their ongoing projects and programs:{" "}
               <Link 
                 href="https://nrd.kbic-nsn.gov/" 
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                KBIC DNR Website
+                KBIC NRD Website
               </Link>
-              <br />
+              <br /><br />
               For questions, contact Erin at{" "}
               <Link href="mailto:erin@nrd.kbic-nsn.gov">erin@nrd.kbic-nsn.gov</Link>.
             </Typography>
@@ -105,11 +110,40 @@ function About() {
             <Typography
               sx={{ fontSize: { xs: '1.125rem', sm: '1.25rem', md: '1.35rem' } }}
             >
-              This project was developed by students from{" "}
+
+              This website was developed by students at Michigan Technological University participating in Senior Project, led
+              by Professor Robert Pastel. 
+
+              <br /><br />
+              Project Members:
+              <br />
+              <ul>
+                <li>Lucas Bohanon</li>
+                <li>Maci Dostaler</li>
+                <li>Peter Laakso</li>
+                <li>Steven Maki</li>
+                <li>Duncan McBride</li>
+                <li>Simon Novak</li>
+              </ul>
+
+              ---
+              <br />
+              This project was initally developed by students from{" "}
               <strong>Michigan Technological University’s CS4760 — Human-Computer Interaction</strong> course. 
               The development team designed and implemented this system to enhance community engagement, promote 
               environmental awareness, and improve accessibility to KBIC and DNR resources.
               <br /><br />
+
+              CS 4760 Team Members:
+              <br />
+              <ul>
+                <li>Nolan Casey</li>
+                <li>Josiah Parrott</li>
+                <li>Nick Abraham</li>
+                <li>Kirsten Love</li>
+              {/*We're missing people, but I don't have the full list. This is pulled from github*/}
+              </ul>
+
               For more details about the course and student projects, visit:{" "}
               <Link 
                 href="https://cs4760.csl.mtu.edu/2025/" 
@@ -118,6 +152,12 @@ function About() {
               >
                 CS4760 Course Website
               </Link>
+
+              <br /><br />
+
+              Special thanks to Tony Garnett for his significant contributions to the original project.
+              Tony is now a graduate student at Michigan Tech.
+
             </Typography>
           </AccordionDetails>
         </Accordion>

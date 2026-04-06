f07a9b160875db335c31eccbd21cf71a50f06173
Duncan M. <161207671+AtlasThe8th@users.noreply.github.com>
2026-04-06 19:40:40 -0400

    Revert "Revert "Frog image changes""

--- src/config.js
+++ src/config.js
@@ -12,91 +12,91 @@ const rawFrogs = [
     // Description and Name are now in en.json under frogs.americantoad
     audio: "/audio/33 American Toad.mp3",
     startTime: 6,
-    image: "/frogs/AmericanToad.jpg"
+    image: "/frogs/newAmericanToad.jpg"
   },
   {
     name: "Bullfrog",
     fieldName: "bullfrog",
     audio: "/audio/01_Bullfrog.mp3",
     startTime: 5,
-    image: "/frogs/Bullfrog.png"
+    image: "/frogs/newAmericanBullfrog.jpg"
   },
   {
     name: "Cope's Gray Treefrog",
     fieldName: "copesgraytreefrog",
     audio: "/audio/21 Cope_s Gray Treefrog.mp3",
     startTime: 6,
-    image: "/frogs/Cope_sGrayTreefrog.jpg"
+    image: "/frogs/newCopesGrayTreeFrog.jpg"
   },
   {
     name: "Eastern Gray Treefrog",
     fieldName: "easterngraytreefrog",
     audio: "/audio/20 Gray Treefrog.mp3",
     startTime: 5,
-    image: "/frogs/EasternGrayTreefrog.jpg"
+    image: "/frogs/newEasternGrayTreeFrog.jpg"
   },
   {
     name: "Fowler's Toad",
     fieldName: "fowlerstoad",
     audio: "/audio/36 Fowler_s Toad.mp3",
     startTime: 5,
-    image: "/frogs/Fowler_sToad.jpg"
+    image: "/frogs/newFowlersToad.jpg"
   },
   {
     name: "Green Frog",
     fieldName: "greenfrog",
     audio: "/audio/03 Green Frog.mp3",
     startTime: 5,
-    image: "/frogs/GreenFrog.jpg"
+    image: "/frogs/newGreenFrog.jpg"
   },
   {
     name: "Mink Frog",
     fieldName: "minkfrog",
     audio: "/audio/05 Mink Frog.mp3",
     startTime: 5,
-    image: "/frogs/MinkFrog.jpg"
+    image: "/frogs/newMinkFrog.jpg"
   },
   {
     name: "Northern Cricket Frog",
     fieldName: "northerncricketfrog",
     audio: "/audio/31 Northern Cricket Frog.mp3",
     startTime: 6,
-    image: "/frogs/NorthernCricketFrog.jpg"
+    image: "/frogs/newNorthernCricketFrog.jpg"
   },
   {
     name: "Northern Leopard Frog",
     fieldName: "northernleopardfrog",
     audio: "/audio/10 Northern Leopard Frog.mp3",
     startTime: 5,
-    image: "/frogs/NorthernLeopardFrog.png"
+    image: "/frogs/newNorthernLeopardFrog.jpeg"
   },
   {
     name: "Pickerel Frog",
     fieldName: "pickerelfrog",
     audio: "/audio/11 Pickerel Frog.mp3",
     startTime: 5,
-    image: "/frogs/PickerelFrog.jpg"
+    image: "/frogs/newPickerelFrog.jpeg"
   },
   {
     name: "Spring Peeper",
     fieldName: "springpeeper",
     audio: "/audio/23 Spring Peeper.mp3",
     startTime: 5,
-    image: "/frogs/SpringPeeper.jpg"
+    image: "/frogs/newSpringPeeper.jpg"
   },
   {
     name: "Western Chorus Frog",
     fieldName: "westernchorusfrog",
     audio: "/audio/27 Western Chorus Frog.mp3",
     startTime: 6,
-    image: "/frogs/WesternChorusFrog.jpg"
+    image: "/frogs/newWesternChorusFrog.jpeg"
   },
   {
     name: "Wood Frog",
     fieldName: "woodfrog",
     audio: "/audio/04 Wood Frog.mp3",
     startTime: 5,
-    image: "/frogs/WoodFrog.jpg"
+    image: "/frogs/newWoodFrog.jpg"
   },
 ];
 
@@ -106,6 +106,12 @@ export const frogContent = {
     audio: frog.audio ? joinBase(frog.audio) : undefined,
     image: frog.image ? joinBase(frog.image) : undefined,
   })),
+  attribution: {
+    text: "Frog images available from ",
+    source: "iNaturalist.org",
+    url: "https://www.inaturalist.org",
+    accessDate: " 3/17/2026"
+  }
 }
 
 
